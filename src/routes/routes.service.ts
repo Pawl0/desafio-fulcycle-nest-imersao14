9f103977a18cf1154510bdbd4f0fb116e61ad58e
Paulo Henrique Oliveira dos Santos <paulo.hosantos@einstein.br>
2023-08-25 13:16:44 -0300

    fix: route service

--- src/routes/routes.service.ts
+++ src/routes/routes.service.ts
@@ -8,17 +8,29 @@ export class RoutesService {
   constructor(private prismaService: PrismaService) { }
 
   async create(createRouteDto: CreateRouteDto) {
-    return this.prismaService.route.create({
+    const { name, source, destination } = createRouteDto;
+
+    const route = await this.prismaService.route.create({
       data: {
-        name: createRouteDto.name,
-        source: createRouteDto.source,
-        destination: createRouteDto.destination,
+        name,
+        source: {
+          create: source,
+        },
+        destination: {
+          create: destination,
+        },
       },
-    })
+    });
+    return route
   }
 
   async findAll() {
-    const routes = await this.prismaService.route.findMany();
+    const routes = await this.prismaService.route.findMany({
+      include: {
+        source: true,
+        destination: true,
+      },
+    });
     return routes
   }
 }
